1353f67dba125f7caea09d6ac93470afbdc4a253
John Oye <olugbengajohnoye@gmail.com>
2024-08-18 07:51:20 +0100

    Added description to the main page for basic SEO

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import { GoogleGeminiEffectDemo } from "@/components/hero";
 
 export const metadata = {
-  title: "Project Hermes"
+  title: "Project Hermes",
+  description: "Project Hermes is a fast key-value store for supercharging your application!"
 }
 
 export default function Home() {
